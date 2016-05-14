a883491b014798a1fdb6bca523bbe2643d2eeaba
Eric Saund <eric.saund@gmail.com>
2016-05-14 11:54:47 -0700

    Initial setup, no code written yet.

---
+++ python/tellPhoneNumber.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python -tt
+
+#tellPhoneNumber.py is an intial start at a dialog management system to
+#transmit structured data between a transmitter and a receiver using human
+#conversational dialogue.
+
+
+import csv
+import random
+import sys
+import copy
+import re
+import os
+import math
+
+
+
+
